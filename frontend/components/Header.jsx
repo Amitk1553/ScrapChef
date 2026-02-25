523bea0f10780ad6b1072c493abd408d9562ed0f
Amitk1553 <amitkumar1553p@gmail.com>
2026-02-25 20:26:49 +0530

    Fix frontend tracking and upload files

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,106 @@
+import {
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  UserButton,
+} from "@clerk/nextjs";
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import { Refrigerator, Sparkles } from "lucide-react";
+import { Cookie } from "lucide-react";
+import UserDropdown from "./UserDropdown";
+import { checkUser } from "@/lib/checkUser";
+import PricingModal from "./PricingModal";
+import { Badge } from "./ui/badge";
+
+const Header = async () => {
+  const user = await checkUser();
+  return (
+    <header
+      className="fixed top-0 w-full border-b border-stone-200 bg-stone-50/80
+    backdrop-blur-md z-50 supports-backdrop-filter:bg-stone-50/60"
+    >
+      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+        <Link href={user ? "/dashboard" : "/"}>
+          <Image
+            src="/logo2.png"
+            alt="ScrapChef Logo"
+            width={60}
+            height={60}
+            className="w-17"
+          />
+        </Link>
+        <div
+          className="hidden md:flex items-center space-x-8 text-sm font-medium
+        text-stone-600"
+        >
+          <Link
+            href="/recipes"
+            className="hover:text-burntOrange-600 transition-colors flex gap-1.5
+            items-center"
+          >
+            <Cookie className="w-4 h-4" />
+            My Recipes
+          </Link>
+          <Link
+            href="/pantry"
+            className="hover:text-burntOrange-600 transition-colors flex gap-1.5
+            items-center"
+          >
+            <Refrigerator className="w-4 h-4" />
+            My Pantry
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            {/* How to Cook */}
+
+            {user && (
+              <PricingModal
+                subscriptionTier={user.subscriptionTier}>
+                    <Badge>
+                        <Sparkles
+                        className={`h-3 w-3 ${
+                            user.subscriptionTier === "pro"
+                            ? "text-white fill-white/20"
+                            : "text-stone-500"
+                        }`}
+                        />
+                        <span>
+                            {user.subscriptionTier === "pro" ? "Pro Chef" : "Free Plan"}
+                        </span>
+                        
+                    </Badge>
+                </PricingModal>
+            )}
+
+            <UserDropdown />
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button
+                variant="ghost"
+                className="text-stone-600 hover:text-burntOrange-600 hover:bg-burntOrange-50
+                font-medium"
+              >
+                Sign In
+              </Button>
+            </SignInButton>
+
+            <SignUpButton mode="modal">
+              <Button variant="primary" className="rounded-full px-6">
+                Get Started
+              </Button>
+            </SignUpButton>
+          </SignedOut>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
